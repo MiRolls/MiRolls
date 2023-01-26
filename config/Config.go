860c8f3f3d326ba25526af1b64ac1a44239e673a
liangmiQwQ <mi_yyds@126.com>
2023-01-26 13:50:48 +0800

    Config update

--- config/Config.go
+++ config/Config.go
@@ -1,4 +1,21 @@
 package config
 
 type Config struct {
+	Server   *Server   `yaml:"server"`
+	Database *Database `yaml:"database"`
+	Site     string
+}
+
+type Server struct {
+	Port   int    `yaml:"port"`
+	Static string `yaml:"static"`
+}
+
+type Database struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Protocol string `yaml:"protocol"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Database string `yaml:"database"`
 }
