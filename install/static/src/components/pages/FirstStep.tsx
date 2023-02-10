5052dd659fa5032ffb6ab77b248a2c2b26d00db5
liangmiQwQ <mi_yyds@126.com>
2023-02-10 22:27:15 +0800

    Installer Update

--- install/static/src/components/pages/FirstStep.tsx
+++ install/static/src/components/pages/FirstStep.tsx
@@ -1,18 +1,49 @@
 import "./global.css"
 import Steps from "../Steps";
-import React, {useImperativeHandle} from "react";
-import ServerInFo from "../ServerInfo";
+import React, {FormEvent, useState} from "react";
+import "../ServerInfo/style.css"
 
-export default function (_: any, ref: React.Ref<unknown> | undefined){
-    useImperativeHandle(ref,()=>({
 
-    }))
+export default function (){
+    interface siteInfo {
+
+        name: string;
+        link: string;
+        logo: string;
+        mainColor: string;
+        icp: string;
+        lang: string;
+        needIcp: boolean;
+        [key: string]: any;
+    }
+
+
+    const [siteInfo,setSiteInfo] = useState<siteInfo>({
+        name: "",
+        link: "",
+        logo: "",
+        mainColor:"",
+        icp:"",
+        lang:"",
+        needIcp: false,
+    })
+
+    function changeInfo(options:string){
+        let siteInfo2 = siteInfo
+        return function (val:FormEvent<HTMLInputElement>){
+            siteInfo2.site[options] = val.currentTarget.value
+            setSiteInfo(siteInfo2)
+        }
+    }
 
     return (
         <div className={"page"}>
             <Steps step={3} CompletedStep={0}></Steps>
             <h1>Enter Your Server Info</h1>
-            <ServerInFo infoName={"Your Site Name"} tips={"Such of Leason's WebSite"}></ServerInFo>
+            <div className={"quest"}>
+                <span>Your Site Name: </span>
+                <input className={"infoType"} placeholder="MiRolls" onInput={changeInfo("name")}></input>.
+            </div>
         </div>
     )
 }
