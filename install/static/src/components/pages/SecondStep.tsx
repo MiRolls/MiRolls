073a326191dee9e9a910ef0cba23d44b109c4d64
liangmiQwQ <mi_yyds@126.com>
2023-02-24 21:59:40 +0800

    dbConfig

--- install/static/src/components/pages/SecondStep.tsx
+++ install/static/src/components/pages/SecondStep.tsx
@@ -1,12 +1,25 @@
 import Steps from "../Steps";
 import React, {useRef} from "react";
 import InputBar from "../InputBar";
+import axios from "axios";
 
 export default function (){
-    const dbUser = useRef(null)
-    const dbPwd = useRef(null)
-    const dbServer = useRef(null)
-    const dbName = useRef(null)
+    interface dbConfig{
+
+    }
+
+    const dbUser = useRef(null);
+    const dbPwd = useRef(null);
+    const dbServer = useRef(null);
+    const dbName = useRef(null);
+
+    function submit(){
+        axios.get("/install/set/database",).then(data => )
+    }
+
+    function getDbConfig(){
+
+    }
 
     return (
         <div className={"page"}>
@@ -18,6 +31,7 @@ export default function (){
             <InputBar placeholder={"mirolls"} question={"Your Database Name: "} ref={dbName}></InputBar>
             <span className={"tips"}>This step is very important. If this info is false, MiRolls will have many error.</span>
             <span className={"tips"}>If you need other settings, you can modify config.yaml</span>
+            <button className={"nextStep"} onClick={submit}>Click To Submit</button>
         </div>
     )
 }
