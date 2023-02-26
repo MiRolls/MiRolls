d255383a1f3fd924e773b7fcf33fcb2a2d1db11b
liangmiQwQ <mi_yyds@126.com>
2023-02-26 15:31:16 +0800

    Dialog close button

--- install/static/src/components/pages/FirstStep.tsx
+++ install/static/src/components/pages/FirstStep.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import {Navigate} from "react-router-dom";
 import InputBar from "../InputBar";
 import mode from "../../mode";
+import Dialog from "../Dialog";
 
 
 export default function (){
@@ -24,6 +25,7 @@ export default function (){
     const [buttonText,setButtonText] = useState("Click To Submit")
     const [isOpacity,setIsOpacity] = useState(1)
     const [isGoNext,setIsGoNext] = useState(<></>)
+    const [dialogError,setDialogError] = useState("")
     const siteName = useRef(null);
     const siteLang = useRef(null);
     const siteLogo = useRef(null);
@@ -40,19 +42,16 @@ export default function (){
                 if(res.data.message === "success"){
                     setIsGoNext(<Navigate to={"/step-two"}/>)
                 }else{
-
+                    setDialogError("Server have some error,please reboot the MiRolls-Program." + res.data.error)
                 }
             }).catch(err=>{
-                alert("Error! Please restart the program." + err)
+                // alert("Error! Please restart the program." + err)
+                setDialogError("Server have some error,please reboot the MiRolls-Program." + err)
             })
         }
     }
 
     function getData():siteInfo{
-    // function getData():{}{
-        // return {
-        //     name:
-        // }
         return {
             name: (siteName.current as any).getAnswer(),
             link: getRootUrl(),
@@ -69,16 +68,23 @@ export default function (){
         return (re.exec(window.location.href) as string[])[0];
     }
 
+    function onDialogClose(){
+        setIsGoNext(<Navigate to={"/"}></Navigate>)
+    }
+
     return (
-        <div className={"page"} style={{opacity:isOpacity}}>
-            <Steps step={3} CompletedStep={0} nowStep={1}></Steps>
-            <h1>Enter Your Server Info</h1>
-            <InputBar placeholder={"MiRolls"} question={"Your Site Name: "} ref={siteName}></InputBar>
-            <InputBar placeholder={'The default theme only supports zh or en'} question={"Your Size Language: "} ref={siteLang}></InputBar>
-            <InputBar placeholder={"https://github.com/fluidicon.png"} question={"Your Size Logo: "} ref={siteLogo}></InputBar>
-            <span className={"tips"}>There all content correspond config.yaml</span>
-            <button className={"nextStep"} onClick={submit}>{buttonText}</button>
-            {isGoNext}
+        <div>
+            <Dialog message={dialogError} type={"error"} title={"Error!"} onCloseToDo={onDialogClose}/>
+            <div className={"page"} style={{opacity:isOpacity}}>
+                <Steps step={3} CompletedStep={0} nowStep={1}></Steps>
+                <h1>Enter Your Server Info</h1>
+                <InputBar placeholder={"MiRolls"} question={"Your Site Name: "} ref={siteName}></InputBar>
+                <InputBar placeholder={'The default theme only supports zh or en'} question={"Your Size Language: "} ref={siteLang}></InputBar>
+                <InputBar placeholder={"https://github.com/fluidicon.png"} question={"Your Size Logo: "} ref={siteLogo}></InputBar>
+                <span className={"tips"}>There all content correspond config.yaml</span>
+                <button className={"nextStep"} onClick={submit}>{buttonText}</button>
+                {isGoNext}
+            </div>
         </div>
     )
 }
