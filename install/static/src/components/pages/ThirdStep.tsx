a13acd34ec75966e160353af8aa4915a28919231
liangmiQwQ <mi_yyds@126.com>
2023-02-26 20:15:21 +0800

    Theme choic

--- install/static/src/components/pages/ThirdStep.tsx
+++ install/static/src/components/pages/ThirdStep.tsx
@@ -1,7 +1,16 @@
-export default function (){
-    return (
-        <div>
+import Steps from "../Steps";
 
+export default function (){
+    function download(){
+
+    }
+
+    return (
+        <div className={"page"}>
+            <Steps step={3} CompletedStep={2} nowStep={3}></Steps>
+            <h1>Choice a theme</h1>
+            <p className={"tips"}>MiRolls before 2.0.0 can only use the default theme. terribly sorry!</p>
+            <button className={"nextStep"} onClick={download}>Download default theme</button>
         </div>
     )
 }
