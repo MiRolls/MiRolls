d8c27477a30a7c3c6026fee70ca5ae31c638f816
liangmiQwQ <mi_yyds@126.com>
2023-02-26 15:38:22 +0800

    Not node_module Folder push

--- install/static/src/components/pages/SecondStep.tsx
+++ install/static/src/components/pages/SecondStep.tsx
@@ -2,15 +2,16 @@ import Steps from "../Steps";
 import React, {useRef, useState} from "react";
 import InputBar from "../InputBar";
 import axios from "axios";
+import Dialog from "../Dialog";
 
-export default function (){
-    interface dbConfig{
-        username:string,
-        password:string,
-        protocol:string,
-        host:string,
-        port:number,
-        database:string,
+export default function () {
+    interface dbConfig {
+        username: string,
+        password: string,
+        protocol: string,
+        host: string,
+        port: number,
+        database: string,
     }
 
     const dbUser = useRef(null);
@@ -19,37 +20,42 @@ export default function (){
     const dbName = useRef(null);
     // refs
 
-    const [isGoNext,setIsGoNext] = useState(<></>)
+    const [isGoNext, setIsGoNext] = useState(<></>)
 
-    function submit(){
-        axios.post("/install/set/database",getDbConfig()).then(res => {
+    function submit() {
+        axios.post("/install/set/database", getDbConfig()).then(res => {
+            if (res.data.message === "success") {
 
+            }
         })
     }
 
-    function getDbConfig():dbConfig{
+    function getDbConfig(): dbConfig {
         return {
-            username:(dbUser.current as any).getAnswer(),
-            password:(dbPwd.current as any).getAnswer(),
-            protocol:"tcp",
-            host:(dbServer.current as any).getAnswer(),
-            port:3306,
-            database:(dbServer.current as any).getAnswer()
+            username: (dbUser.current as any).getAnswer(),
+            password: (dbPwd.current as any).getAnswer(),
+            protocol: "tcp",
+            host: (dbServer.current as any).getAnswer(),
+            port: 3306,
+            database: (dbServer.current as any).getAnswer()
         }
     }
 
     return (
-        <div className={"page"}>
-            <Steps step={3} CompletedStep={1} nowStep={2}></Steps>
-            <h1>Setting Your Database</h1>
-            <InputBar placeholder={"MiRolls"} question={"Your Database Username: "} ref={dbUser}/>
-            <InputBar placeholder={"password112233"} question={"Your Database Password: "} ref={dbPwd}></InputBar>
-            <InputBar placeholder={"localhost"} question={"Your Database Server: "} ref={dbServer}></InputBar>
-            <InputBar placeholder={"mirolls"} question={"Your Database Name: "} ref={dbName}></InputBar>
-            <span className={"tips"}>This step is very important. If this info is false, MiRolls will have many error.</span>
-            <span className={"tips"}>If you need other settings, you can modify config.yaml</span>
-            <button className={"nextStep"} onClick={submit}>Click To Submit</button>
-            {isGoNext}
+        <div>
+            <Dialog message={""} type={"error"} title={"Error!"}></Dialog>
+            <div className={"page"}>
+                <Steps step={3} CompletedStep={1} nowStep={2}></Steps>
+                <h1>Setting Your Database</h1>
+                <InputBar placeholder={"MiRolls"} question={"Your Database Username: "} ref={dbUser}/>
+                <InputBar placeholder={"password112233"} question={"Your Database Password: "} ref={dbPwd}></InputBar>
+                <InputBar placeholder={"localhost"} question={"Your Database Server: "} ref={dbServer}></InputBar>
+                <InputBar placeholder={"mirolls"} question={"Your Database Name: "} ref={dbName}></InputBar>
+                <span className={"tips"}>This step is very important. If this info is false, MiRolls will have many error.</span>
+                <span className={"tips"}>If you need other settings, you can modify config.yaml</span>
+                <button className={"nextStep"} onClick={submit}>Click To Submit</button>
+                {isGoNext}
+            </div>
         </div>
     )
 }
