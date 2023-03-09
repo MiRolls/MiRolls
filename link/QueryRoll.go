13cd487cc2071a78ded3407cc7d3761fcfce02fa
liangmiQwQ <mi_yyds@126.com>
2023-03-09 19:34:42 +0800

    feat:struct

--- link/QueryRoll.go
+++ link/QueryRoll.go
@@ -5,8 +5,26 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
-	"log"
 )
+
+type data struct { //Return's data type
+	Questions      []*Question `json:"questions"`
+	Title          string      `json:"title"`
+	AnswerOfNumber int         `json:"answerOfNumber"`
+}
+
+type Question struct { //Question. return data.
+	Type          string     `json:"type"`
+	OptionsNumber int        `json:"optionsNumber,omitempty"`
+	Placeholder   string     `json:"placeholder,omitempty"`
+	Title         string     `json:"title"`
+	Options       []*Options `json:"options"`
+}
+
+type Options struct {
+	Options     []string
+	Selectivity []int
+}
 
 func QueryRoll(r *gin.Engine) {
 	r.POST("/query/roll", func(c *gin.Context) {
@@ -14,10 +32,13 @@ func QueryRoll(r *gin.Engine) {
 		sql, err := sqlx.Open("mysql", mysql)
 		if err != nil {
 			c.JSON(500, gin.H{"message": err.Error(), "error": err.Error(), "errorType": "database connect error"})
-			log.Fatal("[FATAL ERROR]Cannot connect database")
+			//log.Fatal("[FATAL ERROR]Cannot connect database")
+			return
 		}
+		data := new(data)
 		c.JSON(200, gin.H{
 			"message": "success",
+			"data":    *data,
 		})
 		_ = sql.Close()
 	})
