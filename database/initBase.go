c281818edd96596b9ca81cc9356aaabce701e19f
liangmiQwQ <mi_yyds@126.com>
2023-03-11 20:41:30 +0800

    feat: answer database.

--- database/initBase.go
+++ database/initBase.go
@@ -7,7 +7,8 @@ import (
 	"strings"
 )
 
-const rolls = "CREATE TABLE `rolls` (`id` int(11) NOT NULL AUTO_INCREMENT, `roll` text, `code` text, `link` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4"
+const ROLLS = "CREATE TABLE `rolls` (`id` int(11) NOT NULL AUTO_INCREMENT, `roll` text, `code` text, `link` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4"
+const ANSWER = "CREATE TABLE `answer` (`id` int(11) NOT NULL AUTO_INCREMENT, `answer` text, `link` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf9mb4"
 
 func Init(userName string, password string, ip string, dbName string) error {
 	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", strconv.Itoa(3306), ")/", dbName, "?charset=utf8"}, "")
@@ -19,7 +20,8 @@ func Init(userName string, password string, ip string, dbName string) error {
 		_ = open.Close()
 	}(*db) // Close
 	_, _ = db.Query("DROP TABLE IF EXISTS `rolls`")
-	_, _ = db.Query(rolls)
+	_, _ = db.Query(ROLLS)
+	_, _ = db.Query(ANSWER)
 	_, _ = db.Query("LOCK TABLES `rolls` WRITE")
 	_, _ = db.Query("UNLOCK TABLES;")
 	//Write db
--- link/AnswerQuestionnaire.go
+++ link/AnswerQuestionnaire.go
@@ -6,13 +6,15 @@ import (
 )
 
 type answer struct {
-	Id string `json:"id"`
+	Link   string `json:"link"`
+	Answer string `json:"answer"`
 }
 
 func AnswerQuestionnaire(r *gin.Engine) {
 	r.POST("/answer", func(c *gin.Context) {
 		body, _ := c.GetRawData()
 		answerRel := new(answer)
+
 		_ = json.Unmarshal(body, answerRel)
 	})
 }
