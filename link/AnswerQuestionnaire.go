1828759b8a4ee9eb16608b79d617d7f2b075d8b3
liangmiQwQ <mi_yyds@126.com>
2023-03-11 20:52:04 +0800

    feat: answer database.

--- link/AnswerQuestionnaire.go
+++ link/AnswerQuestionnaire.go
@@ -14,7 +14,6 @@ func AnswerQuestionnaire(r *gin.Engine) {
 	r.POST("/answer", func(c *gin.Context) {
 		body, _ := c.GetRawData()
 		answerRel := new(answer)
-
 		_ = json.Unmarshal(body, answerRel)
 	})
 }
