b7226bb9e910ae70519a55359593d2233ae50778
liangmiQwQ <mi_yyds@126.com>
2023-03-21 19:27:28 +0800

    feat:radio add

--- link/QueryRoll.go
+++ link/QueryRoll.go
@@ -72,10 +72,14 @@ type dbRoll struct {
 type AnswerFromJson struct {
 	Link   string `json:"link"`
 	Answer []struct {
-		Title  string      `json:"title"`
-		Type   string      `json:"type"`
-		Answer interface{} `json:"answer"`
+		Title  string        `json:"title"`
+		Type   string        `json:"type"`
+		Answer []interface{} `json:"answer"`
 	} `json:"answer"`
+}
+
+func (ra *radio) AddNumberOfSelect() {
+	ra.NumberOfSelect++
 }
 
 //goland:noinspection ALL
@@ -121,24 +125,23 @@ func QueryRoll(r *gin.Engine) {
 			data.Questions[i].Title = rollStruct.Quest[i].Title
 			data.Questions[i].Type = rollStruct.Quest[i].Type
 			if data.Questions[i].Type == "radio" || data.Questions[i].Type == "multipleChoice" {
-				//append(data.Questions[i].Answer)
+				//append(data.Questions[answerNumber].Answer)
 				for i2 := 0; i2 < len(rollStruct.Quest[i].Options); i2++ {
 					ra := radio{
 						Option:         rollStruct.Quest[i].Options[i2],
 						NumberOfSelect: 0,
 					}
-
 					data.Questions[i].Answer = append(data.Questions[i].Answer, ra)
 				}
 			}
 		}
 
 		// Use for(){} to loop database.
-		for i := 0; i < len(answers); i++ {
+		for answerNumber := 0; answerNumber < len(answers); answerNumber++ {
 			// loop length for answer.
 			//dbMap := make(map[string]interface{})
 			dbStruct := new(AnswerFromJson)
-			err := json.Unmarshal([]byte(answers[i].Answer), &dbStruct)
+			err := json.Unmarshal([]byte(answers[answerNumber].Answer), &dbStruct)
 			if err != nil {
 				c.JSON(500, gin.H{
 					"error":   "Can't unmarshal json. " + err.Error(),
@@ -146,8 +149,28 @@ func QueryRoll(r *gin.Engine) {
 				})
 				return
 			} // Use json.Unmarshal to parse.
-			data.Questions[i].Title = dbStruct.Answer[i].Title
-			data.Questions[i].Type = dbStruct.Answer[i].Type
+
+			//Loop every question
+			for questionNumber := 0; questionNumber < len(dbStruct.Answer); questionNumber++ {
+				//2 condition
+				if data.Questions[questionNumber].Type == "radio" || data.Questions[questionNumber].Type == "multipleChoice" {
+					for optionNumber := 0; optionNumber < len(dbStruct.Answer[questionNumber].Answer); optionNumber++ {
+						//dbStruct..[questionNumber].Answer[optionNumber]
+						if dbStruct.Answer[questionNumber].Answer[optionNumber] == true {
+							//data.Questions[questionNumber].Answer[optionNumber].numberOfSelect++
+							radio, isOK := data.Questions[questionNumber].Answer[optionNumber].(radio)
+							if isOK {
+								radio.AddNumberOfSelect()
+							} else {
+								c.JSON(500, gin.H{
+									"error":   "data has error!",
+									"message": "error",
+								})
+							}
+						}
+					}
+				}
+			}
 		}
 		// to link rolls
 
